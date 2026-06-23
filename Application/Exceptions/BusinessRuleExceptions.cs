26354a433fc66ab1f04da4c0b9245479084de823
Orkun Keçeli <ork.74@hotmail.com>
2026-06-23 16:18:32 +0300

    Create BusinessRuleExceptions.cs

---
+++ Application/Exceptions/BusinessRuleExceptions.cs
@@ -0,0 +1,11 @@
+﻿namespace SirenStore.Application.Exceptions
+{
+    /// <summary>
+    /// İş kuralı ihlal edildiğinde fırlatılır.
+    /// API katmanında 422 Unprocessable Entity veya 400 Bad Request dönmek için kullanılabilir.
+    /// </summary>
+    public class BusinessRuleException : Exception
+    {
+        public BusinessRuleException(string message) : base(message) { }
+    }
+}
