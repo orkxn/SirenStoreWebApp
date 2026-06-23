58253730355833c3f03c96f1c94c51f9ae83fdbd
Orkun Keçeli <ork.74@hotmail.com>
2026-06-23 16:18:31 +0300

    Create NotFoundExceptions.cs

---
+++ Application/Exceptions/NotFoundExceptions.cs
@@ -0,0 +1,14 @@
+﻿namespace SirenStore.Application.Exceptions
+{
+    /// <summary>
+    /// Aranan kayıt veritabanında bulunamadığında fırlatılır.
+    /// API katmanında 404 Not Found dönmek için kullanılabilir.
+    /// </summary>
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+
+        public NotFoundException(string entityName, long id)
+            : base($"{entityName} bulunamadı. (Id: {id})") { }
+    }
+}
