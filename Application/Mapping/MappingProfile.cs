ffe41b45142b78a9cb2ced45d00480aec84e5117
Orkun Keçeli <ork.74@hotmail.com>
2026-06-23 14:40:57 +0300

    Create MappingProfile.cs

---
+++ Application/Mapping/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Entities.Models;
+using SirenStore.Application.DTOs;
+
+namespace SirenStore.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // CreateSellerDto'yu Seller entity nesnesine çevir (Veri tabanına kaydederken)
+            CreateMap<CreateSellerDto, Seller>();
+
+            // Seller entity nesnesini SellerDto'ya çevir (Angular'a veri gönderirken)
+            CreateMap<Seller, SellerDto>();
+
+
+            // ---------- PRODUCT MODÜLÜ EŞLEŞTİRMELERİ ----------
+            CreateMap<CreateProductDto, Product>();
+            CreateMap<Product, ProductDto>();
+
+
+            // Hatırla: Admin için ayrı tablo yoktu, User tablosunu kullanıyorduk.
+            CreateMap<CreateAdminDto, User>();
+            CreateMap<User, AdminDto>();
+        }
+    }
+}
