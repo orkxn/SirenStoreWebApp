6e7f431168c5a517aababbff1e25cc497651b27d
Orkun Keçeli <ork.74@hotmail.com>
2026-06-23 14:34:03 +0300

    SellerManager Added

--- Application/Services/SellerManager.cs
+++ Application/Services/SellerManager.cs
@@ -1,7 +1,7 @@
 ﻿using AutoMapper;
 using Entities.Models;
 using SirenStore.Application.DTOs;
-using SirenStore.Application.Interfaces; // IRepository'nin olduğu yer
+using SirenStore.Application.Interfaces; 
 
 namespace SirenStore.Application.Services
 {
