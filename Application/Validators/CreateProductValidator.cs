4850fbfe32a0382ca2502e46104024529cc4792d
Orkun Keçeli <ork.74@hotmail.com>
2026-06-23 15:16:08 +0300

    Validators Added

---
+++ Application/Validators/CreateProductValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using SirenStore.Application.DTOs;
+
+namespace SirenStore.Application.Validators
+{
+    public class CreateProductValidator : AbstractValidator<CreateProductDto>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("Ürün adı boş bırakılamaz.")
+                .MaximumLength(200).WithMessage("Ürün adı en fazla 200 karakter olabilir.");
+
+            RuleFor(p => p.Description)
+                .NotEmpty().WithMessage("Ürün açıklaması boş bırakılamaz.");
+
+            RuleFor(p => p.Price)
+                .GreaterThan(0).WithMessage("Ürün fiyatı 0'dan büyük olmalıdır.");
+
+            RuleFor(p => p.Stock)
+                .GreaterThanOrEqualTo(0).WithMessage("Stok adedi negatif olamaz.");
+
+            RuleFor(p => p.SellerId)
+                .GreaterThan(0).WithMessage("Ürünün mutlaka geçerli bir satıcısı (SellerId) olmalıdır.");
+        }
+    }
+}
---
+++ Application/Validators/CreateSellerValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using SirenStore.Application.DTOs;
+
+namespace SirenStore.Application.Validators
+{
+    public class CreateSellerValidator : AbstractValidator<CreateSellerDto>
+    {
+        public CreateSellerValidator()
+        {
+            RuleFor(s => s.StoreName)
+                .NotEmpty().WithMessage("Mağaza adı boş bırakılamaz.")
+                .MinimumLength(3).WithMessage("Mağaza adı en az 3 karakter olmalıdır.")
+                .MaximumLength(150).WithMessage("Mağaza adı en fazla 150 karakter olabilir.");
+
+            RuleFor(s => s.ContactEmail)
+                .NotEmpty().WithMessage("E-posta adresi boş bırakılamaz.")
+                .EmailAddress().WithMessage("Geçersiz e-posta formatı.");
+
+            RuleFor(s => s.ContactPhone)
+                .NotEmpty().WithMessage("İletişim telefonu boş bırakılamaz.")
+                .Matches(@"^\+?[1-9]\d{1,14}$").WithMessage("Geçersiz telefon numarası formatı.");
+        }
+    }
+}
