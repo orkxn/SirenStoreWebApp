7e43dbaa645cf447f1ef580017b1fa214a8a0d3f
Orkun Keçeli <ork.74@hotmail.com>
2026-06-24 09:26:48 +0300

    Update Category.cs

--- Entities/Models/Category.cs
+++ Entities/Models/Category.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema; // [ForeignKey] niteliği için gerekli
 
 namespace Entities.Models
 {
@@ -7,10 +8,14 @@ namespace Entities.Models
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public int? ParentCategoryId { get; set; }
-        public Category ParentCategory { get; set; }
-        public ICollection<Category> SubCategories { get; set; }
+        // 1. DÜZELTME: Tipi int? yerine long? yaptık (BaseModel'deki bigint ID ile eşleşmesi için)
+        public long? ParentCategoryId { get; set; }
 
+        // 2. DÜZELTME: EF Core'a yukarıdaki ID'nin bu nesneye ait olduğunu açıkça belirttik
+        [ForeignKey("ParentCategoryId")]
+        public Category ParentCategory { get; set; }
+
+        public ICollection<Category> SubCategories { get; set; }
         public ICollection<Product> Products { get; set; }
     }
 }
