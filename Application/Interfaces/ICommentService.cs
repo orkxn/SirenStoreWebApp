002324dee9a48754cff5ef0c2e708b8404475f8a
Orkun Keçeli <ork.74@hotmail.com>
2026-06-30 11:50:06 +0300

    Create ICommentService.cs

---
+++ Application/Interfaces/ICommentService.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs.Comment;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public interface ICommentService
+    {
+        Task<IEnumerable<CommentDto>> GetCommentsByProductIdAsync(long productId);
+        Task<CommentDto> CreateCommentAsync(CommentCreateDto dto, long userId);
+        Task<CommentDto> UpdateCommentAsync(long commentId, CommentUpdateDto dto, long userId);
+        Task DeleteCommentAsync(long commentId, long userId, bool isAdmin = false);
+    }
+}
