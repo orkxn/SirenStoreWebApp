72bcb9b528a97aa244677b952c6c9ec04114fd7e
Orkun Keçeli <ork.74@hotmail.com>
2026-06-30 10:28:27 +0300

    Create Comment.cs

---
+++ Entities/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Entities.Models
+{
+    public class Comment : BaseModel
+    {
+        public string Text { get; set; } = null!;
+        public int Rating { get; set; } // 1-5 arası yıldız puanı
+
+        public long UserId { get; set; } // foreign key
+        public User User { get; set; } = null!; 
+
+        public long ProductId { get; set; } // foreign key
+        public Product Product { get; set; } = null!; 
+    }
+}
