9f24da9c33376c98270dfe6e96d7af4fa336488f
Orkun Keçeli <ork.74@hotmail.com>
2026-06-30 15:03:58 +0300

    migrations

---
+++ Infrastructure/Migrations/20260630115624_EmailVerificationFieldsAdded.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmailVerificationFieldsAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email_verification_token",
+                table: "users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "email_verification_token_expiry",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email_verification_token",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "email_verification_token_expiry",
+                table: "users");
+        }
+    }
+}
