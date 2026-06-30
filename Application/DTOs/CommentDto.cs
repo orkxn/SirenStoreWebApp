b61527306a555e2629e7d69f7ce77f3f461e0bd7
Orkun Keçeli <ork.74@hotmail.com>
2026-06-30 11:49:55 +0300

    Create CommentDto.cs

---
+++ Application/DTOs/CommentDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Application.DTOs.Comment
+{
+
+    // ürün hakkındaki yorumun görüntülenecek olan kısmı
+    public class CommentDto
+    {
+        public long Id { get; set; }
+        public string Text { get; set; } = null!;
+        public int Rating { get; set; }
+        public DateTime CreationDate { get; set; }
+        public long UserId { get; set; }
+        public string UserFullName { get; set; } = null!;
+
+        public long ProductId { get; set; }
+    }
+
+    // yeni bir yorum yapılacağı zaman istenen veriler
+    public class CommentCreateDto
+    {
+        public string Text { get; set; } = null!;
+        public int Rating { get; set; }
+        public long ProductId { get; set; }
+    }
+
+    // mevcut yorum güncelleneceği zaman istenen veriler
+    public class CommentUpdateDto
+    {
+        public string Text { get; set; } = null!;
+        public int Rating { get; set; }
+    }
+}
