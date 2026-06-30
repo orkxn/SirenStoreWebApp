773e64eea4e875fd1e09c5494d3a1b9c9e4c166a
Orkun Keçeli <ork.74@hotmail.com>
2026-06-30 10:45:24 +0300

    commentconf added

---
+++ Infrastructure/Configurations/CommentConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SirenStore.Infrastructure.Configurations
+{
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable("comments");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Text)
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            builder.Property(c => c.Rating)
+                .IsRequired();
+
+            builder.Property(c => c.CreationDate)
+                .IsRequired();
+
+            builder.Property(c => c.IsDeleted)
+                .HasDefaultValue(false);
+
+            // bu filtre sayesinde veritabanından çekilen sorgularda IsDeleted = true olan yorumlar otomatik olarak gizlenir.
+            builder.HasQueryFilter(c => !c.IsDeleted);
+
+            // ilişki tanımlamaları (relations)
+
+            // bire çok ilişki user ve comment
+            builder.HasOne(c => c.User)
+                .WithMany(u => u.Comments)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // bire çok ilişki product ve comment
+            builder.HasOne(c => c.Product)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(c => c.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
