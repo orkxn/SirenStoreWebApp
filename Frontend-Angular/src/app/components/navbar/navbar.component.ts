d0400a62dbd34caf83afb49ee99c9255358189ba
Orkun Keçeli <ork.74@hotmail.com>
2026-07-02 14:15:22 +0300

    login-fixed

--- Frontend-Angular/src/app/components/navbar/navbar.component.ts
+++ Frontend-Angular/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { CartService } from '../../services/cart.service';
 import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
@@ -113,7 +113,8 @@ export class NavbarComponent {
 
   constructor(
     public authService: AuthService,
-    private cartService: CartService
+    private cartService: CartService,
+    private router: Router
   ) {}
 
   get cartCount(): number {
@@ -124,6 +125,6 @@ export class NavbarComponent {
   handleLogout() {
     this.authService.logout();
     this.dropdownOpen = false;
-    window.location.href = '/login';
+    this.router.navigate(['/login']);
   }
 }
