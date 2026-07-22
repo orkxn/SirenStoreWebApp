b22ff3720158fdefeb59ccad2509a1ba01719373
Orkun Keçeli <ork.74@hotmail.com>
2026-07-22 14:21:35 +0300

    mobile-deleted

--- mobile/lib/config/api_config.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-import 'package:flutter/foundation.dart';
-
-class ApiConfig {
-  static String get baseUrl => kIsWeb || !Platform.isAndroid
-      ? 'http://localhost:5063/api'
-      : 'http://10.0.2.2:5063/api';
-}
--- mobile/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:siren_store_mobile/main.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const SirenStoreApp());
-    expect(find.text('Siren Store'), findsOneWidget);
-  });
-}
